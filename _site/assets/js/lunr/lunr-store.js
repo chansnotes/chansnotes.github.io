9881966eadeb9a77b4c4a3648116b3b7b9d7bcb6
Seokchan Yoo <stargate0609@gmail.com>
2019-05-02 01:08:39 +0900

    Gem file modification finally...

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "[Vue.js] Vue.js 뽀개기 (1) 개발환경 설정",
+        "excerpt":"[Vue.js] Vue.js 뽀개기 (1) 개발환경 설정 본 글은 Udemy에서 제공하는 유료 강좌인 Vue.js Complete Guide를 수강하면서 정리한 내용입니다. 저작권 문제시 바로 글을 내리도록 하겠습니다.  저는 애기 개발자 입니다. 이전까지 웹 프론트엔드 개발이라고는 약 4년전 석사과정 프로젝트에서 jQuery, Boostrap, 그리고 D3를 사용해서 BeagleBone Black이라는 마이크로컴퓨터에 Web App을 만들어 본 경험이 전부입니다…...","categories": ["vue"],
+        "tags": [],
+        "url": "http://localhost:4000/vue/2019/05/01/vue-tutorial-1.html",
+        "teaser":null}]
